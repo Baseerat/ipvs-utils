aa2a46c3e3b89885038560629e83f57fb83dc6cc
mshahbaz <mshahbaz@cs.princeton.edu>
2017-02-20 19:33:00 -0500

    edits

--- dump-ipvsadm-L-n.py
+++ dump-ipvsadm-L-n.py
@@ -1,5 +1,5 @@
 import subprocess
 
-out_bytes = subprocess.check_output(['watch -n 0.1 ipvsadm -L -n'])
+out_bytes = subprocess.check_output(['watch', '-n 0.1', 'ipvsadm', '-L', '-n'])
 out_bytes_str = out_bytes.decode()
 print(out_bytes_str)
