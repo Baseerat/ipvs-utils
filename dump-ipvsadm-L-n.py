013f1031269710678bc80f0e16d50f01dc05379c
mshahbaz <mshahbaz@cs.princeton.edu>
2017-02-20 20:32:22 -0500

    edits

--- dump-ipvsadm-L-n.py
+++ dump-ipvsadm-L-n.py
@@ -1,26 +1,31 @@
 import subprocess
 import re
 import csv
+import sys
+import time
 
 
-csv_file = "dump-ipvsadm-L-n.csv"
-
-out_bytes = subprocess.check_output(['ipvsadm', '-L', '-n'])
-out_bytes_str = out_bytes.decode()
-# print(out_bytes_str)
-
-hdr = re.findall(r'(RemoteAddress)(:)(Port)'
-                 r'(\s+)(Forward\s+)(Weight)(\s+)(ActiveConn)(\s+)(InActConn)', out_bytes_str)
-stats = re.findall(r'(\d+.\d+.\d+.\d+)(:)(\d+)'
-                   r'(\s+)(Route\s+)(\d+)(\s+)(\d+)(\s+)(\d+)', out_bytes_str)
+if len(sys.argv) < 2:
+    raise Exception('You must pass the timeout value')
+timeout = float(sys.argv[1])
+csv_file = sys.argv[2]
 
 with open(csv_file, 'a') as csv_fd:
-    rst = dict()
-    hdr = hdr[0]
-    for stat in stats:
-        rst[stat[0] + ':' + hdr[5]] = stat[5]
-        rst[stat[0] + ':' + hdr[7]] = stat[7]
-        rst[stat[0] + ':' + hdr[9]] = stat[9]
-    writer = csv.DictWriter(csv_fd, fieldnames=rst.keys())
-    writer.writeheader()
-    writer.writerow(rst)
+    while True:
+        out_bytes = subprocess.check_output(['ipvsadm', '-L', '-n'])
+        out_bytes_str = out_bytes.decode()
+        # print(out_bytes_str)
+
+        stats = re.findall(r'(\d+.\d+.\d+.\d+)(:)(\d+)'
+                           r'(\s+)(Route\s+)(\d+)(\s+)(\d+)(\s+)(\d+)', out_bytes_str)
+
+        rst = dict()
+        for stat in stats:
+            rst[stat[0] + ':Weight'] = stat[5]
+            rst[stat[0] + ':ActiveConn'] = stat[7]
+            rst[stat[0] + ':InActConn'] = stat[9]
+        writer = csv.DictWriter(csv_fd, fieldnames=rst.keys())
+        writer.writeheader()
+        writer.writerow(rst)
+
+        time.sleep(timeout)
